ac4654a1671b749fa66922c6716afae38aa7b01a
tawhid <ah5229530@gmail.com>
2023-10-23 12:32:06 +1100

    added the resurant actitivty as a fragment
    one fragment completed

--- app/src/main/java/com/example/bottomnavyt/MainActivity.kt
+++ app/src/main/java/com/example/bottomnavyt/MainActivity.kt
@@ -1,122 +1,62 @@
 package com.example.bottomnavyt
 
-import android.app.Activity
-import android.content.Intent
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import android.widget.ProgressBar
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.example.bottomnavyt.Restaurant
-import okhttp3.*
-import org.json.JSONArray
-import org.json.JSONObject
-import java.io.IOException
-import okhttp3.Callback
-import okhttp3.Call
-import okhttp3.OkHttpClient
-import okhttp3.Request
-import okhttp3.Response
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var recyclerView: RecyclerView
-    private lateinit var progressBar: ProgressBar
-    private lateinit var getLocationButton: Button
-    private lateinit var adapter: RestaurantAdapter
 
-    private val LOCATION_REQUEST_CODE = 1
-
-    private var apiKey = "AIzaSyCG-YhCR3j6oq8Av3Jz78OuTrjbUnzLtzI"
-    private var location: String = ""
-    private var radius: Int = 1500
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navView: NavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        recyclerView = findViewById(R.id.restaurantRecyclerView)
-        progressBar = findViewById(R.id.progressBar)
-        getLocationButton = findViewById(R.id.getLocationButton)
-        adapter = RestaurantAdapter()
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
 
-        // Set up RecyclerView
-        val layoutManager = LinearLayoutManager(this)
-        recyclerView.layoutManager = layoutManager
-        recyclerView.adapter = adapter
+        drawerLayout = findViewById(R.id.drawer_layout)
+        navView = findViewById(R.id.nav_view)
 
-        getLocationButton.setOnClickListener {
-            val locationIntent = Intent(this, LocationActivity::class.java)
-            startActivityForResult(locationIntent, LOCATION_REQUEST_CODE)
-        }
-    }
+        // Initialize the ActionBarDrawerToggle instance
+        val toggle = ActionBarDrawerToggle(
+            this, drawerLayout, toolbar,
+            R.string.navigation_drawer_open, R.string.navigation_drawer_close
+        )
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
 
-        if (requestCode == LOCATION_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
-            val latitude = data?.getStringExtra("latitude")
-            val longitude = data?.getStringExtra("longitude")
-            val radiusStr = data?.getStringExtra("radius")
-
-            if (latitude != null && longitude != null && radiusStr != null) {
-                location = "$latitude, $longitude"
-                radius = radiusStr.toInt()
-                getNearbyRestaurants(apiKey, location, radius, adapter)
-            }
-        }
-    }
-
-    private fun getNearbyRestaurants(apiKey: String, location: String, radius: Int, adapter: RestaurantAdapter) {
-        val client = OkHttpClient()
-        val url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json" +
-                "?location=$location" +
-                "&radius=$radius" +
-                "&type=restaurant" +
-                "&key=$apiKey"
-
-        val request = Request.Builder()
-            .url(url)
-            .build()
-
-        progressBar.visibility = View.VISIBLE
-
-        client.newCall(request).enqueue(object : Callback {
-            override fun onResponse(call: Call, response: Response) {
-                if (response.isSuccessful) {
-                    val responseData = response.body?.string()
-                    val restaurants = parseRestaurantData(responseData)
-                    runOnUiThread {
-                        adapter.setData(restaurants)
-                        progressBar.visibility = View.GONE
-                    }
-                } else {
-                    // Handle error response
+        navView.setNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.nav_your_fragment -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container, Resturants()).commit()
                 }
+                // Handle other navigation items if added
             }
+            drawerLayout.closeDrawer(GravityCompat.START)
+            true
+        }
 
-            override fun onFailure(call: Call, e: IOException) {
-                // Handle network or request failure
-            }
-        })
+        // If you want to set a default fragment to load on app start:
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, Resturants()).commit()
+            navView.setCheckedItem(R.id.nav_your_fragment)
+        }
     }
 
-    private fun parseRestaurantData(responseData: String?): List<Restaurant> {
-        val restaurantList = mutableListOf<Restaurant>()
-        responseData?.let {
-            val jsonObject = JSONObject(it)
-            val results = jsonObject.getJSONArray("results")
-
-            for (i in 0 until results.length()) {
-                val restaurant = results.getJSONObject(i)
-                val name = restaurant.getString("name")
-                val vicinity = restaurant.getString("vicinity")
-
-                val newRestaurant = Restaurant(name, vicinity)
-                restaurantList.add(newRestaurant)
-            }
+    override fun onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
         }
-        return restaurantList
     }
 }
