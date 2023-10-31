03038aa4bcdfaca311c474d22a81041ca567b480
tawhid <ah5229530@gmail.com>
2023-10-31 21:34:19 +1100

    update
    update

--- app/src/main/java/com/example/bottomnavyt/SettingsFragment.kt
+++ app/src/main/java/com/example/bottomnavyt/SettingsFragment.kt
@@ -1,4 +1,5 @@
 package com.example.bottomnavyt
+
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -6,12 +7,10 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.appcompat.widget.SwitchCompat
 import androidx.appcompat.app.AppCompatDelegate
-import com.example.bottomnavyt.R
 
 class SettingsFragment : Fragment() {
 
-    private lateinit var switchLightMode: SwitchCompat
-    private lateinit var switchDarkMode: SwitchCompat
+    private lateinit var switchTheme: SwitchCompat
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,30 +18,31 @@ class SettingsFragment : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.settings_fragment, container, false)
 
-        switchLightMode = view.findViewById(R.id.switch_light_mode)
-        switchDarkMode = view.findViewById(R.id.switch_dark_mode)
+        switchTheme = view.findViewById(R.id.switch_theme)
 
-        // Check and set the initial state of the toggle buttons
+        // Check and set the initial state of the toggle button
         val currentNightMode = AppCompatDelegate.getDefaultNightMode()
-        switchLightMode.isChecked = currentNightMode == AppCompatDelegate.MODE_NIGHT_NO
-        switchDarkMode.isChecked = currentNightMode == AppCompatDelegate.MODE_NIGHT_YES
+        switchTheme.isChecked = currentNightMode == AppCompatDelegate.MODE_NIGHT_YES
+        updateSwitchText()
 
-        // Set click listeners for the toggle buttons
-        switchLightMode.setOnCheckedChangeListener { _, isChecked ->
+        // Set click listener for the toggle button
+        switchTheme.setOnCheckedChangeListener { _, isChecked ->
             if (isChecked) {
-                // Set the app's theme to Light Mode
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            } else {
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
             }
-        }
-
-        switchDarkMode.setOnCheckedChangeListener { _, isChecked ->
-            if (isChecked) {
-                // Set the app's theme to Dark Mode
-                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
-            }
+            updateSwitchText()
         }
 
         return view
     }
-}
 
+    private fun updateSwitchText() {
+        if (switchTheme.isChecked) {
+            switchTheme.text = "Dark Mode"
+        } else {
+            switchTheme.text = "Light Mode"
+        }
+    }
+}
